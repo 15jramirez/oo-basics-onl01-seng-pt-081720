57df4dd4d965fe4dba7cbb1bd428f0e34bef1f3e
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-09-04 03:43:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,12 @@
 require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition, :brand
-  def initialize (brand)
+  def initialize (brand, condition="new")
     @brand = brand
    end
    def cobble
      if condition != "new"
        puts "Your shoe is as good as new!"
-       condition ="new"
      end
    end
-          condition ="new"
 end 
